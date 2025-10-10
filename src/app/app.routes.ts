83d2917cd684ff0578c1d3c6f4f5c60b4da89495
Katharina <katharinahoelzlvienna@gmail.com>
2025-10-10 10:40:59 +0200

    Set up Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Cart } from './cart/cart';
+import { About } from './about/about';
+import { Products } from './products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'cart', component: Cart },
+  { path: 'about', component: About },
+  { path: 'menu', component: Products },
+];
